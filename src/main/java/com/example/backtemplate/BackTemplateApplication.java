d66920bbc65950e94421c3f82378eb5c352a3da8
Breno Alvim <alvimbrenosilva6@gmail.com>
2026-07-09 22:10:36 -0300

    chore: scaffold Maven/Spring Boot project

---
+++ src/main/java/com/example/backtemplate/BackTemplateApplication.java
@@ -0,0 +1,11 @@
+package com.example.backtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTemplateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackTemplateApplication.class, args);
+    }
+}
